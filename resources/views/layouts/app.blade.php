1a9cba1a8b65f642d41266ebd26dee22e54a2535
Simen Daehlin <simen.dehlin@gmail.com>
2017-07-28 22:11:05 +0100

    Added navbar, fontawesome and styles

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,11 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>{{config('app.name', 'Laravel Blog')}}</title>
+        <link rel="stylesheet" href="{{asset('css/app.css')}}">
     <body>
+        @include('inc.navbar')
+    <div class="container">
         @yield('content')
+        </div>
     </body>
 </html>
--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -1,6 +1,9 @@
     @extends('layouts.app')
     
     @section('content')
-        <h1>{{$title}}</h1>
-        <p>This is an application from the built with Laravel from scratch</p>
+       <div class="jumbotron text-center">
+           <h1>Welcome to a Laravel App</h1>
+           <p>This is the Laravel application i built from scratch</p>
+           <p><a class="btn btn-primary btn-lg" href="/login" role="button"><i class="fa fa-key"></i> Login</a> <a class="btn btn-success btn-lg"href="/register" role="button"><i class="fa fa-user-plus"></i> Register</a></p>
+       </div>
     @endsection
