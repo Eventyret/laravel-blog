afa93ce79311a9f24b301dc3f114ae73e90f8431
Simen Daehlin <simen.dehlin@gmail.com>
2017-07-28 21:20:09 +0100

    Init Commit

---
+++ resources/views/pages/index.blade.php
@@ -0,0 +1,12 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>{{config('app.name', 'Laravel Blog')}}</title>
+    <body>
+    <h1>Welcome to Laravel</h1>
+    <p>This is an application from the "Laravel from Scratch"</p>
+    </body>
+</html>
---
+++ routes/web.php
@@ -0,0 +1,25 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+// Route::get('/users/{id}/{name}', function($id, $name){
+//     return 'This is user ' .$name. ' with an id of ' .$id;
+// });
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/', 'PagesController@index');
+
+Route::get('/about', function(){
+    return view('pages.about');
+});
